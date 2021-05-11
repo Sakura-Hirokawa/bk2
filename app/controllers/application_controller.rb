851c43875b6c3eed8bb0db0bf91704737b78c791
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-11 09:14:51 +0000

    [Add]before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
+  # top.aboutアクションを除いてログイン
   before_action :authenticate_user!, except: [:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  # サインインしたらログインしたユーザー画面
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
-
+  
+  # ログアウトしたらトップ画面へ
   def after_sign_out_path_for(resource)
     root_path
   end
